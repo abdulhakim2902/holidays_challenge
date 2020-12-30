83ed83ec4dfe3e2d2935158437a8490b50cf74ec
abdulhakim2902 <abdulhakim2902@gmail.com>
2020-12-30 10:46:06 +0700

    migration-complete-release-2

---
+++ migrations/20201230033201-remove-account-number-from-customers.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const account = require("../models/account");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.removeColumn('Customers','accountNumber')
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.addColumn('Customers', 'accountNumber', {
+      type:Sequelize.STRING
+    })
+  }
+};
